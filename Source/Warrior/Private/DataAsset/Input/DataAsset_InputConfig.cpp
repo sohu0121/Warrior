fb802cc9dc737f80c63b02cee55d86a5254ef50e
sohu0121 <sohu0121@gmail.com>
2025-03-04 23:18:05 +0900

    Input Config Data Asset

---
+++ Source/Warrior/Private/DataAsset/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAsset/Input/DataAsset_InputConfig.h"
+
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InputTag)
+{
+	for (const FWarriorInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	return nullptr;
+}

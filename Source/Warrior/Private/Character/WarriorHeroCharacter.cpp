783f5ddc8db4383692fa1f7f36c0e24af9711fd6
sohu0121 <sohu0121@gmail.com>
2025-03-04 18:04:35 +0900

    Debug Helper

--- Source/Warrior/Private/Character/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Character/WarriorHeroCharacter.cpp
@@ -2,4 +2,11 @@
 
 
 #include "Character/WarriorHeroCharacter.h"
+#include "WarriorDebugHelper.h"
 
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Debug::Print(TEXT("Working"));
+}
--- Source/Warrior/Public/Character/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Character/WarriorHeroCharacter.h
@@ -13,5 +13,7 @@ UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;
 };

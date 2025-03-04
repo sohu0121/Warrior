7b32db03743c9d9b8ac63cb661968e758acc8fcc
sohu0121 <sohu0121@gmail.com>
2025-03-04 18:20:01 +0900

    Hero Camera Setting

--- Source/Warrior/Public/Character/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Character/WarriorHeroCharacter.h
@@ -6,14 +6,29 @@
 #include "Character/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AWarriorHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	
+#pragma region Components
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	UCameraComponent* FollowCamera;
+	
+#pragma endregion
 };

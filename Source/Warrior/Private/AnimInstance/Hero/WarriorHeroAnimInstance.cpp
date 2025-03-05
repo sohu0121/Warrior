ef67fb476c4f46880299aaf244e36eb35d7aca6c
sohu0121 <sohu0121@gmail.com>
2025-03-05 21:57:08 +0900

    Hero Animation Instance

--- Source/Warrior/Private/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,30 @@
 
 #include "AnimInstance/Hero/WarriorHeroAnimInstance.h"
 
+#include "Character/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaTime);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaTime;
+		bShouldEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
--- Source/Warrior/Public/AnimInstance/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstance/Hero/WarriorHeroAnimInstance.h
@@ -6,12 +6,26 @@
 #include "AnimInstance/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
-/**
- * 
- */
+class AWarriorHeroCharacter;
+
 UCLASS()
 class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="AnimData|Referece")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElapsedTime;
 };
